4cb44885b011f4843d56737de3343f34c99c61b4
khannaanurag1 <khannaanurag1@gmail.com>
2024-05-13 09:24:21 +0530

    Added BCryptPasswordEncoder and Spring Security

--- src/main/java/org/example/userauthenticationservice_april/services/AuthService.java
+++ src/main/java/org/example/userauthenticationservice_april/services/AuthService.java
@@ -3,6 +3,7 @@ package org.example.userauthenticationservice_april.services;
 import org.example.userauthenticationservice_april.models.User;
 import org.example.userauthenticationservice_april.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -13,6 +14,9 @@ public class AuthService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
     public User signUp(String email, String password) {
         Optional<User> optionalUser = userRepository.findByEmail(email);
         if(optionalUser.isPresent()) {
@@ -21,7 +25,7 @@ public class AuthService {
 
         User user = new User();
         user.setEmail(email);
-        user.setPassword(password);
+        user.setPassword(bCryptPasswordEncoder.encode(password));
         userRepository.save(user);
         return user;
     }
---
+++ src/main/java/org/example/userauthenticationservice_april/springsecurity/SecurityConfig.java
@@ -0,0 +1,24 @@
+package org.example.userauthenticationservice_april.springsecurity;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain getSecurityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.cors().disable();
+        httpSecurity.csrf().disable();
+        httpSecurity.authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll());
+        return  httpSecurity.build();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder getBcryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
